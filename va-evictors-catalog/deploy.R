64d7b564602006c49e190fd587d5c5254cf2e51c
Michele Claibourn <mclaibourn@virginia.edu>
2022-11-02 16:11:58 -0400

    add deploy file

---
+++ va-evictors-catalog/deploy.R
@@ -0,0 +1,14 @@
+rsconnect::deployApp(appName = "va-evictors-catalog", 
+                     appFiles = c("app.R", "app-template.html", 
+                                  "app-datanotes", "app-orient-notes",
+                                  "app-project-notes", 
+                                  "plaintiff-aggregated-data.txt",
+                                  "monthly-plaintiff-aggregated-data.txt", 
+                                  "yearl-plaintiff-aggregated-data.txt", 
+                                  "www/style.css", "www/plaintiff_courtnamesonly.csv",
+                                  "www/EquityCenter-Logo_Typed_White.png", "www/eclogo.png", 
+                                  "www/GitHub-Mark-Light-64px.png", 
+                                  "www/rvalogo-removebg.png", "rvalogo.jpg",
+                                  "www/va-evictors-preview.png"),
+                     account = "virginiaequitycenter") 
+
